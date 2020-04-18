c13f2c27b3278b2edf6cd570677f51829c55f990
dyobi <dyobi.92@gmail.com>
2020-04-18 16:29:00 -0700

    .

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -12,11 +12,19 @@
 
 #include "../includes/ft_printf.h"
 
+int		get_valid(t_info *info, const char *s)
+{
+	info->f = (*s == 88) ? info->f | CT_HEXUP : info->f;
+	info->f = (*s == 120) ? info->f | CT_HEXDN : info->f;
+	return ((*s == 37 || *s == 88 || *s == 99 || *s == 100 || *s == 102 || \
+		*s == 105 || *s == 112 || *s == 115 || *s == 117 || *s == 120) ? 1 : 0);
+}
+
 int		ft_main(t_info *info, const char *s)
 {
 	while (*s)
 	{
-		if (*s == '%')
+		if (*s == 37)
 		{
 			s = get_info(info, ++s);
 			if (!get_valid(info, s))
