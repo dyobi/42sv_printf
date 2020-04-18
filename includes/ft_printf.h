f67820eee84f3e87a9a858e463916573963cb004
dyobi <dyobi.92@gmail.com>
2020-04-17 22:27:01 -0700

    .

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -13,4 +13,46 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+// FLAGS
+
+# define F_SPACE 1
+# define F_PLUS 2
+# define F_MINUS 4
+# define F_ZERO 8
+# define F_HASH 16
+
+// WIDTH & PRECISION
+
+# define WIDTH 32
+# define PRECI 64
+
+// LENGTH_TYPE
+
+# define LT_L 128
+# define LT_LL 256
+# define LT_H 512
+# define LT_HH 1024
+
+// CONVERSION_TYPE
+
+# define CT_HEXUP 2048
+# define CT_HEXDN 4096
+
+typedef struct	s_dispatch_table
+{
+	char	specifier;
+	int		(*ft)();
+}				t_dt;
+
+typedef struct	s_info
+{
+	va_list	ap;
+	int		f;
+	int		w;
+	int		w_s;
+	int		p;
+	int		p_s;
+	int		len;
+}				t_info;
+
 #endif
